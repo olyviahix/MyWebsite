7ab2f137ad8edf333c5a5ac603de4fa76575f4eb
Olyvia Hix <olyvia4@gmail.com>
2022-10-25 19:24:40 -0700

    code for today

---
+++ programming fundimentals/array.js
@@ -0,0 +1,7 @@
+let n = [10, 4, 54, 23, 5]
+for(i=0; i < n.length; i++){
+    let big = n.[0];
+    if(big > n[i]) {
+        if
+    }
+}
---
+++ programming fundimentals/interactive.js
@@ -0,0 +1,7 @@
+let ans = 0;
+for(a=0; a<5; a++)
+{
+    console.log("before addition ans = "+ans+ " a = "+a);
+        ans=ans+a;
+        console.log("after addition ans = "+ans;
+console.log(ans);
